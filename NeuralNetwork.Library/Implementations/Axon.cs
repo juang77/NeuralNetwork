2683c797a25ebcdc65bfe64be3770f80d5ba27c5
Juan Gabriel Gomez Baron <juang77@hotmail.com>
2025-09-03 19:42:11 -0500

    Axon and Neuron implementation

---
+++ NeuralNetwork.Library/Implementations/Axon.cs
@@ -0,0 +1,22 @@
+﻿using NeuralNetwork.Abstration;
+
+namespace NeuralNetwork.Library.Implementations
+{
+    public class Axon : IAxon
+    {
+        public List<ISynapse> Terminals { get; } = [];
+
+        public void AddTerminal(ISynapse terminal)
+        {
+            Terminals.Add(terminal);
+        }
+
+        public void SendOutputValueToTerminal(double value)
+        {
+            foreach (ISynapse terminal in Terminals)
+            {
+                terminal.ReceiveInputValue(value);
+            }
+        }
+    }
+}
---
+++ NeuralNetwork.Library/Implementations/Neuron.cs
@@ -0,0 +1,43 @@
+﻿using NeuralNetwork.Abstration;
+
+namespace NeuralNetwork.Library.Implementations
+{
+    public class Neuron(IInputFunction inputFunction, IActivationFunction activationFunction) : INeuron
+    {
+        public IEnumerable<ISynapse> Dendrites => DendritesField;
+
+        private readonly List<ISynapse> DendritesField = new();
+
+        public IAxon Axon { get; } = new Axon();
+
+        public double OutputValue { get; private set; }
+
+        public double Bias { get; private set; }
+
+        public IInputFunction InputFunction => inputFunction;
+
+        public IActivationFunction Activationfunction() => activationFunction;
+       
+        public void AddDendrite(ISynapse dendrite)
+        {
+            dendrite.OnInputValueReceived += dendrite_OnInputValueReceived;
+            DendritesField.Add(dendrite);
+        }
+
+        public void AddTerminal(ISynapse terminal) => Axon.AddTerminal(terminal);
+
+        public void SetBiasValue(double value)
+        {
+            Bias = value;
+        }
+
+        private void dendrite_OnInputValueReceived(ISynapse dendrite)
+        {
+            double inputValue = inputFunction != default ? inputFunction.CalculateInput(DendritesField, Bias) : dendrite.Value;
+
+            OutputValue = activationFunction != default ? activationFunction.CalculateOutput(inputValue) : inputValue;
+
+            Axon.SendOutputValueToTerminal(OutputValue);
+        }
+    }
+}
